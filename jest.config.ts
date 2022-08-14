9fd9b931044124c9ae12d7756a251dd8e002dcc4
stunerx <cleyton.a.santos@gmail.com>
2022-08-14 13:15:44 -0300

    chore: jest config

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  clearMocks: true,
+  coverageProvider: "v8",
+  preset: "ts-jest",
+  rootDir: ".",
+  testRegex: ".*\\..*spec\\.ts$",
+  transform: {
+    "ˆ.+\\.ts?$": ["@swc/jest"],
+  },
+};
